ce967b81d0d4cd911da38ff68ae9d72f3c68563d
futuroDevLeo <leonunes07@outlook.com>
2024-12-17 16:19:35 -0300

    init: initial commit :tada:

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # conversorDeUnidades
 Programa Conversor de Unidades em Linguagem C.
+
+Para assegurar a assimilação dos conceitos iniciais de versionamento e do uso do Git e GitHub, propõe-se a criação de um repositório colaborativo. O objetivo principal é desenvolver habilidades práticas nessas ferramentas, com ênfase no trabalho em equipe e na colaboração. A atividade consistirá na configuração de um projeto simples, permitindo a simulação de um fluxo de trabalho real com versionamento.
---
+++ src/main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+
+// 1. Unidades de comprimento
+//
+//
+//
+//
+//
+// 2. Unidades de massa
+//
+//
+//
+//
+//
+// 3. Unidades de volume
+//
+//
+//
+//
+//
+// 4. Unidades de temperatura
+//
+//
+//
+//
+//
+// 5. Unidades de velocidade
+//
+//
+//
+//
+//
+// 6. Unidades de potência
+//
+//
+//
+//
+//
+// 7. Unidades de área
+//
+//
+//
+//
+//
+// 8. Unidades de tempo (segundos, minutos, horas)
+//
+//
+//
+//
+//
+// 9. Unidades de armazenamento de dados (Bits, bytes, kilobytes (KB), megabytes (MB), gigabytes (GB), terabytes (TB))
+//
+//
+//
+//
+//
+// 10. Interface de usuário
+//
+//
+//
+//
+//
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
