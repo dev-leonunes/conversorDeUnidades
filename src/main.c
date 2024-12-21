634cb8d85c8a04aada3932f6b5aad8aac540a724
IagoMartinsDev <iagoomartiinss@hotmail.com>
2024-12-21 17:42:53 -0300

    Exercício 3 - Unidades de Volume

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <math.h>
+#include <string.h>
 
 // 1. Unidades de comprimento
 //
@@ -14,11 +14,46 @@
 //
 //
 // 3. Unidades de volume
-//
-//
-//
-//
-//
+double converterVolume(double valorConvertido, int unidadeOrigem, int unidadeDestino) {
+
+    // l = litros
+    // m = mililitros
+    // c = metros cubicos
+
+    // validação de erro de entrada
+    if ((unidadeOrigem != 'l' && unidadeOrigem != 'm' && unidadeOrigem != 'c') ||
+        (unidadeDestino != 'l' && unidadeDestino != 'm' && unidadeDestino != 'c')) {
+        return -1; 
+    }
+
+    // usando litros como valor base
+    double litros;
+
+    if (unidadeOrigem == 'l') {
+        litros = valorConvertido;
+    } else if (unidadeOrigem == 'm') {
+        litros = valorConvertido / 1000.0;
+    } else if (unidadeDestino == 'c') {
+        litros = valorConvertido * 1000.0;
+    }
+
+    if (unidadeDestino == 'l') {
+        return litros;
+    } else if (unidadeDestino == 'm') {
+        return litros * 1000.0;
+    } else if (unidadeDestino == 'c') {
+        return litros / 1000.0;
+    }
+
+    return -1;
+}
+
+int main() {
+// Testes 
+    printf("5 litros = %.2f mililitros\n", converterVolume(5, 'l', 'm'));
+    printf("3000 mililitros = %.2f litros\n", converterVolume(3000, 'm', 'l'));
+}
+
 // 4. Unidades de temperatura
 //
 //
@@ -62,8 +97,8 @@
 //
 //
 
-int main()
-{
-    printf("Hello, World!\n");
-    return 0;
-}
+//int main()
+//{
+//    printf("Hello, World!\n");
+//    return 0;
+//}
