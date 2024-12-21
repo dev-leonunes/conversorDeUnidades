ea3961b7a6902ce45edc1525f0fba00ce4d322b4
ALrEcompUefs <contaofcali@gmail.com>
2024-12-21 12:46:38 -0300

    função conversão de comprimento
    implementação da função de conversão de unidades de comprimento

--- src/main.c
+++ src/main.c
@@ -1,31 +1,61 @@
 #include <stdio.h>
 #include <math.h>
 
+// cria uma enumeração para a função de conversão de unidades de medida
+enum Comprimentos{
+    Km =1,
+    Hm,
+    Dam,
+    M,
+    Dm,
+    Cm,
+    Mm
+};
 // 1. Unidades de comprimento
-    // Recebe o valor em metros(float) e retorna o valor em centimetros
-    float metro2cent(float medida){
-        return medida*100.0;
-    }
-    // Recebe o valor em metros(float) e retorna o valor em milimetros
-    float metro2mili(float medida){
-        return medida*1000.0;
-    }
+    /* Conversor de unidades de comprimento
+        |   Recebe uma medida de comprimento converte para outro multiplo ou submultiplo, ex Km->m
+        |   Paramêtros:
+        |               -> comprimento(float): o valor de compriemnto a ser convertido
+        |               -> escala(int): a escala de comprimento da medida, os valores foram definidos em
+        |                   em uma enumeração como mostrado abaixo
+        |                   Km:  escala em quilomêtros
+        |                   Hm:  escala em hectomêtros
+        |                   Dam: escala decâmetros
+        |                   M:   escala em metros
+        |                   Dm:  escala e decómetros
+        |                   Cm:  escala em centímetros 
+        |                   mm:  escala em milimetros
+        |               -> nova_escala(int): a escala de  comprimento  a ser convertida
+        |                   Km:  escala em quilomêtros
+        |                   Hm:  escala em hectomêtros
+        |                   Dam: escala decâmetros
+        |                   M:   escala em metros
+        |                   Dm:  escala e decómetros
+        |                   Cm:  escala em centímetros 
+        |                   mm:  escala em milimetros
+        |   Retorno:
+        |               -> comprimento_convertido(float): o valor da medida na nova escala
+        */
+    float conversor_comprimento(float comprimento,int escala,int nova_escala){
+        float comprimento_convertido=0;
+        int diferenca =0; // recebe a diferença de posições entre as escalas
+        // Determina o fator de conversão pelas potências de 10
 
-    // Recebe o valor em centimetros(float) e retorna o valor em metros
-    float cent2metro(float medida){
-        return medida/100.0;
-    }
-    // Recebe o valor em centimetros(float) e retorna o valor em milimetros
-    float cent2mili(float medida){
-        return medida*10.0;
-    }
-    // Recebe o valor em milimetros(float) e retorna o valor em metros
-    float mili2metro(float medida){
-        return medida/1000.0;
-    }
-    // Recebe o valor em milimetros(float) e retorna o valor em centimetros
-    float mili2cent(float medida){
-        return medida/10.;
+        // se a escala atual tem um valor menor que a nova escala
+        if(escala < nova_escala){
+            diferenca = nova_escala - escala;
+            comprimento_convertido = comprimento * (1.0*pow(10,diferenca));
+        }
+        // se a escala atual tem uma valor maior que a nova escala
+        else if (escala > nova_escala){
+            diferenca = escala- nova_escala;
+            comprimento_convertido = comprimento /(1.0*pow(10,diferenca));
+        }
+        // caseo sejam as mesmas esalas então nada se altera
+        else{
+            comprimento_convertido = comprimento;
+        }
+        return comprimento_convertido;
     }
 //
 //
